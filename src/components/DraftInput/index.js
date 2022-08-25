b38e9ba71c59a794878b7e790b8fdb767cf9c3fe
Jorge Felix <jorge30fm@gmail.com>
2022-08-25 15:13:39 -0400

    align elements on page using bootstrap

--- src/components/DraftInput/index.js
+++ src/components/DraftInput/index.js
@@ -6,7 +6,7 @@ function DraftInput() {
 	return (
 		<Container>
 			<Form>
-				<Row className="justify-content-center">
+				<Row className="justify-content-center mt-5 mb-4">
 					<Col xs={9}>
 						<Form.Label>Title</Form.Label>
 					</Col>
@@ -14,27 +14,29 @@ function DraftInput() {
 						<Form.Control type="text"></Form.Control>
 					</Col>
 				</Row>
-				<Row className="justify-content-center">
+				<Row className="justify-content-center mb-5">
 					<Col xs={9}>
 						<Form.Label>Content Description</Form.Label>
 					</Col>
 					<Col xs={9}>
-						<Form.Control type="text"></Form.Control>
+						<Form.Control as="textarea" rows={8}></Form.Control>
 					</Col>
 				</Row>
-				<Row className="justify-content-around">
-					<Col xs={3}>
+				<Row className="d-flex justify-content-center mb-5">
+					<Col xs={2} className="text-center">
 						<Button>S</Button>
 					</Col>
-					<Col xs={3}>
+					<Col xs={2} className="text-center">
 						<Button>M</Button>
 					</Col>
-					<Col xs={3}>
+					<Col xs={2} className="text-center">
 						<Button>L</Button>
 					</Col>
 				</Row>
 				<Row className="justify-content-center">
-					<Col xs={6}><Button>Compose</Button></Col>
+					<Col xs={6} className="text-center">
+						<Button >Compose</Button>
+					</Col>
 				</Row>
 			</Form>
 		</Container>
