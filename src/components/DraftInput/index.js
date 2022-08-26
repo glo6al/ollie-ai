8417f6a1ae201583815d60b5f4d6d032c3dbccc1
Jorge Felix <jorge30fm@gmail.com>
2022-08-25 21:10:40 -0400

    add styling

--- src/components/DraftInput/index.js
+++ src/components/DraftInput/index.js
@@ -8,34 +8,41 @@ function DraftInput() {
 			<Form>
 				<Row className="justify-content-center mt-5 mb-4">
 					<Col xs={9}>
-						<Form.Label>Title</Form.Label>
+						<Form.Label className="heading mb-3">Title</Form.Label>
 					</Col>
 					<Col xs={9}>
-						<Form.Control type="text"></Form.Control>
+						<Form.Control type="text" className="form"></Form.Control>
 					</Col>
 				</Row>
 				<Row className="justify-content-center mb-5">
 					<Col xs={9}>
-						<Form.Label>Content Description</Form.Label>
+						<Form.Label className="heading mb-3">Content Description</Form.Label>
 					</Col>
 					<Col xs={9}>
-						<Form.Control as="textarea" rows={8}></Form.Control>
+						<Form.Control as="textarea" rows={8} className="form"></Form.Control>
 					</Col>
 				</Row>
 				<Row className="d-flex justify-content-center mb-5">
-					<Col xs={2} className="text-center">
-						<Button>S</Button>
+					<Col xs={9}>
+						<Form.Label className="heading mb-4">Output Length</Form.Label>
 					</Col>
-					<Col xs={2} className="text-center">
-						<Button>M</Button>
-					</Col>
-					<Col xs={2} className="text-center">
-						<Button>L</Button>
+					<Col xs={12}>
+						<Row className="d-flex justify-content-center">
+							<Col xs={3} className="text-center">
+								<button className="sq-btn button">S</button>
+							</Col>
+							<Col xs={3} className="text-center">
+								<button className="sq-btn button">M</button>
+							</Col>
+							<Col xs={3} className="text-center">
+								<button className="sq-btn button">L</button>
+							</Col>
+						</Row>
 					</Col>
 				</Row>
 				<Row className="justify-content-center">
 					<Col xs={6} className="text-center">
-						<Button >Compose</Button>
+						<button className="button">Compose</button>
 					</Col>
 				</Row>
 			</Form>
