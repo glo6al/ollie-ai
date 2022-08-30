18243762c679a6051bc24d75613c82e7f9e52f04
glo6al <towers.a@gmail.com>
2022-08-30 11:11:23 -0400

    render text area and compose button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import DraftInput from './components/DraftInput';
-import Dashboard from './components/Dashboard';
-import AddClient from './components/AddClient';
-import Header from './components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import DraftInput from "./components/DraftInput";
+import Dashboard from "./components/Dashboard";
+import AddClient from "./components/AddClient";
+import Header from "./components/Header";
+import Compose from "./components/Compose";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <main>
-        <Dashboard/>
+        <Dashboard />
         <DraftInput></DraftInput>
-        <AddClient/>
+        <AddClient />
+        <Compose />
       </main>
     </div>
   );
--- src/components/Compose/index.js
+++ src/components/Compose/index.js
@@ -1,2 +1,32 @@
 import React from "react";
-import { Form, Container, Row, Col } from "react-bootstrap";
+import { Form, Container, Row, Col, FormGroup } from "react-bootstrap";
+
+function Compose() {
+  return (
+    <Container>
+      <Form>
+        <Row id="docEditor" className="justify-content-center">
+          <Col xs={9}>
+            <Form.Control
+              as="textarea"
+              rows={12}
+              className="form"
+              contentEditable="true"
+              data-gramm="false"
+              data-placeholder="Begin writing the workout..."
+            ></Form.Control>
+          </Col>
+          <Row className="justify-content-center">
+            <Col xs={6} className="text-center">
+              <button type="submit" className="button" content="">
+                Compose
+              </button>
+            </Col>
+          </Row>
+        </Row>
+      </Form>
+    </Container>
+  );
+}
+
+export default Compose;
